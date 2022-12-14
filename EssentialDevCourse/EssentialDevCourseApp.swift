01408516d73bce82a7e2d5d64a6753b7b6fff3d2
Tanya Landsman <tlandsman@activecampaign.com>
2022-12-14 09:13:48 -0600

    Initial Commit

---
+++ EssentialDevCourse/EssentialDevCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialDevCourseApp.swift
+//  EssentialDevCourse
+//
+//  Created by Tanya Landsman on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct EssentialDevCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
