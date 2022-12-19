74b431948a0961e68ae9452cc07369da59ee23aa
Tanya Landsman <tlandsman@activecampaign.com>
2022-12-19 11:43:58 -0600

    Create test helpers

--- EssentialDevCourseTests/Networking/RemoteFeedLoaderTests.swift
+++ EssentialDevCourseTests/Networking/RemoteFeedLoaderTests.swift
@@ -24,31 +24,36 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-  
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        let url = URL(string: "https://a-url.com")!
-        let _ = RemoteFeedLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
     }
     
+    //MARK: Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (RemoteFeedLoader, HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        return (RemoteFeedLoader(url: url, client: client), client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+      
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+
 }
 
