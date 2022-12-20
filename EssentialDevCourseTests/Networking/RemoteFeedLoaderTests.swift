52f5a2e7419786523eb7accfa396678f90471a26
Tanya Landsman <tlandsman@activecampaign.com>
2022-12-20 09:53:31 -0600

    Move Remote Feed Loader components to production

--- EssentialDevCourseTests/Networking/RemoteFeedLoaderTests.swift
+++ EssentialDevCourseTests/Networking/RemoteFeedLoaderTests.swift
@@ -7,22 +7,7 @@
 
 import Foundation
 import XCTest
-
-class RemoteFeedLoader {
-    let client: HTTPClient
-    let url: URL
-    init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    func load() {
-       client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
+import EssentialDevCourse
 
 
 class RemoteFeedLoaderTests: XCTestCase {
