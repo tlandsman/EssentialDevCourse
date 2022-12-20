1e51e0432d8c994af03c8238965702281271cfc6
Tanya Landsman <tlandsman@activecampaign.com>
2022-12-20 10:16:27 -0600

    Guarrantee correct number of times client is called.

--- EssentialDevCourseTests/Networking/RemoteFeedLoaderTests.swift
+++ EssentialDevCourseTests/Networking/RemoteFeedLoaderTests.swift
@@ -16,7 +16,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
@@ -25,8 +25,18 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURL, url)
     }
     
-    //MARK: Helpers
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
     
+    //MARK: Helpers
+
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (RemoteFeedLoader, HTTPClientSpy) {
         let client = HTTPClientSpy()
         return (RemoteFeedLoader(url: url, client: client), client)
@@ -34,9 +44,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
-      
+        var requestedURLs = [URL]()
+        
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 
