d624c763908071dc39874b1692c5f646eceedc12
Tanya Landsman <tlandsman@activecampaign.com>
2023-01-12 09:27:56 -0600

    Add comment

--- EssentialDevCourse/Feed Api/RemoteFeedLoader.swift
+++ EssentialDevCourse/Feed Api/RemoteFeedLoader.swift
@@ -31,6 +31,7 @@ public final class RemoteFeedLoader {
         client.get(from: url, completion: { result in
             switch result {
             case let .success(data, response):
+                // We use a static function on this helper to avoid capture self if it was an instance method.  this is key because if the instance of the Remote Feed Loader has been deallocated this block can still be invoked causing a bug.  We don't know the implementtion of the client.  It might be a singleton that lives longer than the remote feed loader. Consumers of the remote feed loader might not expect the completion block to be invoked after the Remote Feeder has been deallocated. 
                 let result = FeedItemsMapper.map(data, from: response)
                 return completion(result)
             case .failure:
